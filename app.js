1a343a26d766a3175c097b2c041ab454bea4b67c
rvanooyen <robert.vanooyen@gmail.com>
2021-09-26 18:23:03 -0500

    feature/questions

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-// const printProfileData = profileDataArgs => {
-//     // This...
-//     for (let i = 0; i < profileDataArgs.length; i++) {
-//         console.log(profileDataArgs[i]);
-//     }
-    
-//     console.log('================')
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-//     // Is the same as this...
-//     profileDataArgs.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
