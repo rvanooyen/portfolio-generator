81dc123c4a9ff1b637d5e7c40f6b9709be013b3d
rvanooyen <robert.vanooyen@gmail.com>
2021-09-26 21:44:54 -0500

    complete feature/updated-template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -132,7 +132,13 @@ const promptProject = (portfolioData) => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', generatePage(portfolioData), err => {
+             if (err) throw new Error(err);
+
+             console.log('Portfolio complete! Check out index.html to see the output!');
+        });        
     });    
 
 // const pageHTML = generatePage(name, github);
